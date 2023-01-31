f377352720eb6479234f41dece0b8f68425faa02
MuhammedAba <talhaaba456@gmail.com>
2023-02-01 01:03:13 +0300

    create a model and field

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Hospital Management',
+    'version' : '1.1.0',
+    'category': 'Hospital',
+    'summary': 'Hospital Management System',
+    'sequence': -100,
+    'description': """Hospital Managemetn System""",
+    'category': '',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'author': 'Muhammed Aba',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
